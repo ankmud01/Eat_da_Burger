02c5156930cde787c34850a80c6a23c8ad57c5ba
Rebel Soul <mudvari.ankit@gmail.com>
2020-05-28 00:07:02 -0400

    need to work on handlebars and ui js file..

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,44 @@
+// $(function(){
+//     $(".change-devour").on("Click", function(event){
+//         let id = $(this).data("id");
+
+//         let newDevour = $(this).data("newdevour");
+//         var newDevourState = {
+//             devoured: newDevour
+//         };
+
+//         $.ajax("/api/burgers/" + id, {
+//             type: "PUT",
+//             data: newDevourState
+//         }).then(
+//             function(){
+//                 console.log(`changed devoured to ${newDevour}`);
+//                 location.reload();
+//             }
+//         );
+//     });
+
+//     $(".create-form").on("submit", function(event) {
+//         // Make sure to preventDefault on a submit event.
+//         event.preventDefault();
+    
+//         let newBurger = {
+//           burger_name: $("#bur").val().trim(),
+//           devoured: $("[name=devoured]:checked").val().trim()
+//         };
+
+//         console.log(newBurger);
+    
+//         // Send the POST request.
+//         $.ajax("/api/burgers", {
+//           type: "POST",
+//           data: newBurger
+//         }).then(
+//           function() {
+//             console.log("created new burger");
+//             // Reload the page to get the updated list
+//             location.reload();
+//           }
+//         );
+//       });
+// })
